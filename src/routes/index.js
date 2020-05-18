aad1ac9013f01f03d5d7817d40d60b544348e5c5
Phil Wee <cyber_peewee@yahoo.com>
2020-05-18 01:40:09 -0700

    09-middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
